d6f34cac40940414f8b47a5a0210c2385d51440c
xq <xq123@qq.com>
2020-09-01 15:17:01 +0800

    第二次提交 分类页面

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,112 @@
 // pages/category/index.js
+
+import { request } from "../../request/index.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 左侧的菜单数据
+    leftMenuList: [],
+    // 右侧的商品数据
+    rightContent: [],
+    // 被点击的左侧菜单
+    currenIndex: 0,
+    // 右侧内容的滚动条距离顶部的距离
+    scrollTop:0
   },
+  Cates: [],
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 1.先判断一下本地存储中有没有旧数据
+    // {time:Data,now(),data:[...]}
+    // 2.没有旧数据 直接发送新请求
+    // 3.有旧数据 同时 旧数据没有过期 就使用 本地存储中的旧数据
+
+    // 1.获取本地存储中的数据 （小程序也是存储在本地存储 技术）
+    const Cates = wx.getStorageSync("cates");
+    // 2.判断
+    if(!Cates){
+      // 不存在 发送请求获取数据
+      this.getCates();
+    }else{
+      // 有旧的数据 定义过期时间 10s改成5分钟
+      if(Date.now()-Cates.time>1000*10){
+        // 重新发送请求
+        this.getCates();
+      }else{
+        this.Cates =Cates.data;
+
+        let leftMenuList = this.Cates.map(v => v.cat_name);
+
+        let rightContent = this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      
+      }
+      
+    }
 
   },
+  async getCates() {
+    // request({
+    //   url: "/categories"
+    // })
+    //   .then(res => {
+    //     this.Cates = res.data.message;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    //     // 把接口的数据存入到本地中
+    //     wx.getStorageSync("cates",{time:Date.now(),data:this.Cates})
 
+    //     //构造左侧的大菜单数据
+    //     let leftMenuList = this.Cates.map(v => v.cat_name);
+
+    //     // 构造右侧商品数据
+    //     let rightContent = this.Cates[0].children;
+    //     this.setData({
+    //       leftMenuList,
+    //       rightContent
+    //     })
+    //   })
+    // 1.使用es7的async await来发送请求
+    const res = await request({url:"/categories"})
+    this.Cates = res;
+
+        // 把接口的数据存入到本地中
+        wx.getStorageSync("cates",{time:Date.now(),data:this.Cates})
+
+        //构造左侧的大菜单数据
+        let leftMenuList = this.Cates.map(v => v.cat_name);
+
+        // 构造右侧商品数据
+        let rightContent = this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
   },
+  // 左侧菜单的点击事件
+  handleItemTap(e) {
+    // console.log(e); 
+    // 1.获取被点击身上的索引
+    // 2.给data中的currentIndex赋值
+    // 3.根据不同的索引来渲染右侧的商品内容  
+    const { index } = e.currentTarget.dataset;
+ 
+    let rightContent = this.Cates[index].children;
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.setData({
+      currenIndex: index,
+      rightContent,
+     // 重新设置 右侧内容的scroll-view标签的距离顶部的距离
+      scrollTop:0
+    })
+   
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -33,30 +33,30 @@ Page({
   },
   // 获取轮播图的数据
   getSwiperList(){
-    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"})
+    request({url:"/home/swiperdata"})
     .then(result=>{
       this.setData({
-        swiperList:result.data.message
+        swiperList:result
       })
     })
   },
 
   // 获取分类航的数据
   getCateList(){
-    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"})
+    request({url:"/home/catitems"})
     .then(result=>{
       this.setData({
-        cateList:result.data.message
+        cateList:result
       })
     })
   },
 
    // 获取楼层的数据
   getFloorList(){
-    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/floordata"})
+    request({url:"/home/floordata"})
     .then(result=>{
       this.setData({
-        floorList:result.data.message
+        floorList:result
       })
     })
   }
